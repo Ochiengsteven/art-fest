5631c78e72a8c436327c9ef221432c5d98e1efef
Ochiengsteven <stevenochieng6305@gmail.com>
2023-04-25 16:40:50 +0300

    add custom html and styles for the desktop view

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const mainMenu = document.querySelector('.mainMenu');
 const closeMenu = document.querySelector('.closeMenu');
 const openMenu = document.querySelector('.openMenu');
+const home = document.querySelector('a[href="#about"]');
+const program = document.querySelector('a[href="#program"]');
 
 function show() {
   mainMenu.style.display = 'flex';
@@ -13,3 +15,5 @@ function close() {
 
 openMenu.addEventListener('click', show);
 closeMenu.addEventListener('click', close);
+program.addEventListener('click', close);
+home.addEventListener('click', close);
